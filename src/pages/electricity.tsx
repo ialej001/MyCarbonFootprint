7cf92203e369a4a424e7767667158f78dfe904a0
Ticia Dunn <dunnticia633@gmail.com>
2022-08-26 20:50:48 +0000

    Feature Create frontend UI for electricity calculator     - added type for CountryCode in Electricty schema     - Created Response and Form components for Electricity pages     - Created json file to contain states and provinces for dropdown     - Added states.json file containing states and provinces data

--- src/pages/electricity.tsx
+++ src/pages/electricity.tsx
@@ -1,7 +1,27 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import { trpc } from '../utils/trpc';
+import ElectricityForm from '../components/Electricity/ElectricityForm';
+import { UnregisteredElectricityRequest } from '../schema/electricity.schema';
+import ElectricityResponse from '../components/Electricity/ElectricityResponse';
+import { Spinner } from 'flowbite-react';
 
 export default function ElectricityConsumption() {
+  const { mutate, data, isLoading } = trpc.useMutation([
+    'electricity.unregistered-request-electricity',
+  ]);
+  const handleSubmit = (electricityData: UnregisteredElectricityRequest) => {
+    mutate({ ...electricityData });
+  };
+
+  if (isLoading) {
+    return (
+      <div className="mx-auto">
+        <Spinner aria-label="Electricity response loading" />
+      </div>
+    );
+  }
+
   return (
     <div>
       <Head>
@@ -11,7 +31,9 @@ export default function ElectricityConsumption() {
       <nav className="flex justify-center gap-4">
         <Link href="/">Home</Link>
       </nav>
-      <h2>Electricity Consumption Page</h2>
+
+      {!data && !isLoading && <ElectricityForm handleSubmit={handleSubmit} />}
+      {data && !isLoading && <ElectricityResponse data={data} />}
     </div>
   );
 }
