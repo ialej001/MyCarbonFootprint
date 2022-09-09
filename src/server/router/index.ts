4a10dcde0ed92ecfc959859c13f0e3bc08e5a053
ialej001 <ivan.alejandre@gmail.com>
2022-09-09 11:22:29 -0700

    even more build fixes

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,8 +1,5 @@
 // src/server/router/index.ts
 import { createRouter } from "./context";
-import superjson from "superjson";
-
-import { exampleRouter } from './example';
 import { protectedExampleRouter } from './protected-example-router';
 import { electricityRouter } from './electricity.router';
 import { flightRouter } from "./flight.router";
@@ -11,7 +8,6 @@ import { fuelRouter } from './fuel.router';
 import { preferencesRouter } from "./preferences.router";
 
 export const appRouter = createRouter()
-  .merge('example.', exampleRouter)
   .merge('question.', protectedExampleRouter)
   .merge('electricity.', electricityRouter)
   .merge('flight.', flightRouter)
